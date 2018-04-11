de82cc7df4f54947b6b3b4b966f28791b144f3e0
SANO Akira <sano@maica.org>
2018-04-12 02:35:32 +0900

    done.

--- ble_sample/ViewController.swift
+++ ble_sample/ViewController.swift
@@ -21,53 +21,56 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralMa
     override func viewDidLoad() {
         super.viewDidLoad()
         logView.text = "Start BLE_Sample.app\n"
-        // セントラルマネージャ、ペリフェラルマネージャの初期化
+        // セントラルマネージャの初期化
         centralManager = CBCentralManager(delegate: self, queue: nil)
         //peripheralManager = CBPeripheralManager(delegate: self, queue: nil, options: nil)
-
-        Thread.sleep(forTimeInterval: 10.0)
-        // セントラルのスキャン開始
-        centralManager.scanForPeripherals(withServices: nil, options: nil)
-        logView.insertText("Start Central Scan\n")
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
     @IBAction func toggleCentral(_ sender: Any) {
         if centralSW.isOn {
             // ペリフェラルのアドバタイズ停止
-            //peripheralManager.stopAdvertising()
-            //logView.insertText("Stop Peripheral Advertising\n")
+            peripheralManager.stopAdvertising()
+            logView.insertText("Stop Peripheral Advertising\n")
             // セントラルの再初期化
-            //centralManager = CBCentralManager(delegate: self, queue: nil)
-            // セントラルのスキャン開始
-            centralManager.scanForPeripherals(withServices: nil, options: nil)
-            logView.insertText("Start Central Scan\n")
-
+            centralManager = CBCentralManager(delegate: self, queue: nil)
         } else {
             // セントラルのスキャン停止
             centralManager.stopScan()
             logView.insertText("Stop Central Scan\n")
             // ペリフェラルの再初期化
-            //peripheralManager = CBPeripheralManager(delegate: self, queue: nil, options: nil)
-            // ペリフェラルのアドバタイズ開始
-            //let advertisementData = [CBAdvertisementDataLocalNameKey: "BLE Sample"]
-            //peripheralManager.startAdvertising(advertisementData)
-            //logView.insertText("Start Peripheral Advertising\n")
+            peripheralManager = CBPeripheralManager(delegate: self, queue: nil, options: nil)
         }
     }
     // セントラルマネージャの状態変化を取得
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         logView.insertText("central state: \(central.state)\n")
+        if central.state == .poweredOn {
+            // セントラルのスキャン開始
+            centralManager.scanForPeripherals(withServices: nil, options: nil)
+            logView.insertText("Start Central Scan\n")
+        }
     }
     // セントラルマネージャのスキャン結果を取得
     func centralManager(_ central: CBCentralManager,
                         didDiscover peripheral: CBPeripheral,
                         advertisementData: [String : Any], rssi RSSI: NSNumber) {
-        logView.insertText("advertisementData: \(advertisementData)\n")
-        //logView.insertText("peripheral: \(peripheral)\n")
+        logView.insertText("peripheral: \(peripheral)\n")
+        let range = NSMakeRange(logView.text.characters.count - 1, 0)
+        logView.scrollRangeToVisible(range)
     }
     // ペリフェラルマネージャの状態変化を取得
     func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
         logView.insertText("peripheral state: \(peripheral.state)\n")
+        if peripheral.state == .poweredOn {
+            // ペリフェラルのアドバタイズ開始
+            let advertisementData = [CBAdvertisementDataLocalNameKey: "BLE Sample"]
+            peripheralManager.startAdvertising(advertisementData)
+            logView.insertText("Start Peripheral Advertising\n")
+        }
     }
     // ペリフェラルのアドバタイズ開始処理結果を取得
     func peripheralManagerDidStartAdvertising(_ peripheral: CBPeripheralManager, error: Error?) {
@@ -77,12 +80,5 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralMa
         }
         logView.insertText("Advertising Succeeded\n")
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
