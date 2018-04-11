e848c6afaa19ba122080245631652322ca7fe6ca
SANO Akira <sano@maica.org>
2018-04-11 21:41:50 +0900

    Initial Commit

---
+++ ble_sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ble_sample
+//
+//  Created by sano on 20180411.
+//  Copyright © 2018年 slab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
